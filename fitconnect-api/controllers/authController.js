d38bb838c2a7ff5e0217b9cdc80dcea8293e0bf3
Kyle Williams <43432040+kyledwilly@users.noreply.github.com>
2025-02-18 13:19:18 -0700

    1st commit
    1st commit

---
+++ fitconnect-api/controllers/authController.js
@@ -0,0 +1,9 @@
+exports.login = async (req, res) => {
+    // Handle login logic
+    res.send('Login successful');
+};
+
+exports.register = async (req, res) => {
+    // Handle registration logic
+    res.send('Registration successful');
+};
---
+++ fitconnect-api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Define a route for the root path
+app.get('/', (req, res) => {
+  res.send('Welcome fdfdfdfdfdto FitConnect API!');
+});
+
+// Use your other routes (e.g., auth routes)
+app.use('/api/auth', authRoutes);
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
